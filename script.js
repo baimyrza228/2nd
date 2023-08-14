7b97b6a796563cb63fe80f321f0737865277f076
baimyrza228 <139465998+baimyrza228@users.noreply.github.com>
2023-08-14 22:32:06 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+//switch
+//case sluchay
+//defualt
+//break - stop!
+
+// let a = prompt('2+2 = How much will be?')
+
+// switch (Number(a)) {
+//     case 3:
+//         alert('Not yet')
+//         break;
+//     case 4:
+//         alert('That is true')
+//         break;
+//     case 5:
+//         alert('It is too much')
+//         break;
+//     default:
+//         alert('It is can not be like this')
+// }
+
+
+//function funkcia
+
+//declariton function
+//argument dokozatyel'stva
+function salamber(arg1,arg2,arg3) {
+    alert(arg1+" "+arg2+" and "+arg3 + ' U are studying very nice')
+}
+salamber('Askar','Nurgit','Nuremir')
+// salamber()
